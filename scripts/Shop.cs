b53989e5322035b1ea7262a2b797de6316c99df8
Bubseatbubs <bubseatbubs@gmail.com>
2025-08-25 13:20:28 -0700

    Began working on shop scene

---
+++ scripts/Shop.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class Shop : Control
+{
+    [Export] VBoxContainer shopChoices;
+    [Export] PackedScene shopCardScene;
+    [Export] int choicesAvailable = 5;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        int _upgradeID;
+        for (int i = 0; i < choicesAvailable; i++)
+        {
+            _upgradeID = GD.RandRange(0, 100); // TODO: update w/ ids in-game
+            CreateOffer(_upgradeID);
+        }
+    }
+
+    public void CreateOffer(int id)
+    {
+        ShopCard card = shopCardScene.Instantiate<ShopCard>();
+        card.AssignUpgrade(id);
+
+        shopChoices.AddChild(card);
+    }
+}
