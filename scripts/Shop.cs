b235b348fa2019e833c89b6fda66db1c3e76d02b
Kofu <kofu145g@gmail.com>
2025-08-26 01:06:19 -0700

    refactor: pull changes from cardimpl branch

--- scripts/Shop.cs
+++ scripts/Shop.cs
@@ -3,31 +3,45 @@ using System;
 
 public partial class Shop : Control
 {
-	[Export] VBoxContainer shopChoices;
-	[Export] PackedScene shopCardScene;
-	[Export] int choicesAvailable = 5;
+    [Export] VBoxContainer shopChoices;
+    [Export] PackedScene shopCardScene;
+    [Export] int choicesAvailable = 5;
 
-	public override void _Ready()
-	{
-		base._Ready();
-		Initialize();
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+        Initialize();
+    }
 
-	public void Initialize()
-	{
-		int _upgradeID;
-		for (int i = 0; i < choicesAvailable; i++)
-		{
-			_upgradeID = GD.RandRange(0, 100); // TODO: update w/ ids in-game
-			CreateOffer(_upgradeID);
-		}
-	}
+    public void Initialize()
+    {
+        int _upgradeID;
+        for (int i = 0; i < choicesAvailable; i++)
+        {
+            _upgradeID = GD.RandRange(0, 100); // TODO: update w/ ids in-game
+            CreateOffer(_upgradeID);
+        }
+    }
 
-	public void CreateOffer(int id)
-	{
-		ShopCard card = shopCardScene.Instantiate<ShopCard>();
-		card.AssignUpgrade(id);
+    public void Clear()
+    {
+        foreach (ShopCard card in shopChoices.GetChildren())
+        {
+            card.RemoveFromShop();
+        }
+    }
 
-		shopChoices.AddChild(card);
-	}
+    public void CreateOffer(int id)
+    {
+        ShopCard card = shopCardScene.Instantiate<ShopCard>();
+        card.AssignUpgrade(id);
+
+        shopChoices.AddChild(card);
+    }
+
+    public void Reroll()
+    {
+        Clear();
+        Initialize();
+    }
 }
