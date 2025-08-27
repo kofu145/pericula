e2f5a2d7fbac898055da5944840513ff56d041c1
Kofu <kofu145g@gmail.com>
2025-08-26 23:06:38 -0700

    feat(DeckManager): initial deck fields to manage, plus hands and disc

--- scripts/shared/DeckManager.cs
+++ scripts/shared/DeckManager.cs
@@ -3,6 +3,32 @@ using System;
 
 public partial class DeckManager : Node
 {
-	public Deck PlayerDeck;
+    public static readonly int seed = 100000;
+
+    [Export]
+
+    [Export]
+    public Deck EnemyDeck;
+
+    // Intermediary collections used when actually battling
+    private Godot.Collections.Array<CardData> playerBattleDeck;
+    private Godot.Collections.Array<CardData> enemyBattleDeck;
+
+    private bool initialized;
+
+    public readonly Random RndGen = new(seed);
+    public Godot.Collections.Array<CardData> Hand = new();
+    public Godot.Collections.Array<CardData> EnemyHand = new();
+
+    public Godot.Collections.Array<CardData> playerDisc = new();
+    public Godot.Collections.Array<CardData> enemyDisc = new();
+
+
+    public override void _Ready()
+    {
+        initialized = false;
+        Shuffle(true);
+        Shuffle(false);
+    }
 
 }
