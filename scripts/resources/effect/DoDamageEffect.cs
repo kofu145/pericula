52b81c1c09399ea64088946eb5e55444ac81df9e
Kofu <kofu145g@gmail.com>
2025-08-26 18:34:14 -0700

    feat(test effects): additional card effects to test adding to carddata list

---
+++ scripts/resources/effect/DoDamageEffect.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class DoDamageEffect : EffectTemplate
+{
+    [Export] public int Damage;
+    public override void OnUse(EffectParam param)
+    {
+        param.Target.HP -= Damage;
+    }
+}
---
+++ scripts/resources/effect/TestEffect.cs
@@ -0,0 +1,8 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class TestEffect : EffectTemplate
+{
+    public override void OnUse(EffectParam param) { }
+}
