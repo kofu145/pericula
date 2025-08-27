660e63d70cb4ad853511eb7f540d58f22627bd63
Kofu <kofu145g@gmail.com>
2025-08-27 03:35:08 -0700

    feat(BattleState): edit for new lane changes

--- scripts/combat/BattleState.cs
+++ scripts/combat/BattleState.cs
@@ -6,6 +6,14 @@ public partial class BattleState : Node
 	private int playerBet = 0;
 	private int enemyBet = 0;
 	public int Pot => playerBet + enemyBet;
-	public Godot.Collections.Array<CardData> PlayerLane;
-	public Godot.Collections.Array<CardData> EnemyLane;
+	public CardLane PlayerLane;
+	public CardLane EnemyLane;
+	public Turn currentTurn;
+
+	public void Initialize(CardLane playerLane, CardLane enemyLane)
+	{
+		PlayerLane = playerLane;
+		EnemyLane = enemyLane;
+		currentTurn = Turn.Player;
+	}
 }
