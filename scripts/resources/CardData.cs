87e4ed4c4feb8849c9299324b70559a1fee81915
Kofu <kofu145g@gmail.com>
2025-08-26 23:01:19 -0700

    feat(CardData): updated with id field for future ref

--- scripts/resources/CardData.cs
+++ scripts/resources/CardData.cs
@@ -9,6 +9,7 @@ public partial class CardData : Resource
     [Export] public int MaxHP;
     [Export] public int BaseAttack;
     [Export] public int Attack;
+    [Export] public int id;
     [Export] public Image Texture;
     [Export] public Godot.Collections.Array<EffectTemplate> OnUse;
     [Export] public Godot.Collections.Array<EffectTemplate> Passives;
