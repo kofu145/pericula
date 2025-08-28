b9a1098e3886bb87f4ebf33a7378382d4bb74294
Bubseatbubs <bubseatbubs@gmail.com>
2025-08-28 14:02:15 -0700

    Feat: Add pawn cards to cardData and update cardData to include description, rarity, and traits

--- scripts/resources/CardData.cs
+++ scripts/resources/CardData.cs
@@ -5,6 +5,12 @@ using Godot.Collections;
 public partial class CardData : Resource
 {
 	[Export] public string DisplayName;
+	[Export] public string Description;
+
+	[Export(PropertyHint.Enum, "Common,Rare,Mythic,Legendary,Starter,Token")]
+	public string Rarity;
+	[Export(PropertyHint.Enum, "Knight,Arcane,Citizen,Royalty,Beast,Mechanical,WildCard,Pawn")]
+	public string Trait;
 	[Export] public int HP;
 	[Export] public int MaxHP;
 	[Export] public int BaseAttack;
@@ -13,4 +19,9 @@ public partial class CardData : Resource
 	[Export] public Image Texture;
 	[Export] public Array<EffectTemplate> OnUse;
 	[Export] public Array<EffectTemplate> Passives;
+
+	public override string ToString()
+	{
+		return $"{DisplayName} ({id}):\nHealth: {MaxHP}, Attack: {BaseAttack}, Trait: {Trait}, Rarity: {Rarity}, Description: {Description}";
+	}
 }
--- scripts/shared/CardLookup.cs
+++ scripts/shared/CardLookup.cs
@@ -1,11 +1,13 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class CardLookup : Node
 {
 	public static CardLookup Instance { get; private set; }
 
 	const string CARD_DATA_PATH = "res://resources/cardData/";
+	const int TOTAL_CARD_COUNT = 4;
 
 	public override void _Ready()
 	{
@@ -29,5 +31,24 @@ public partial class CardLookup : Node
 
 		GD.PrintErr("Couldn't find card with ID: " + id);
 		return null;
+	}
+
+	public static List<CardData> GetCardList()
+	{
+		List < CardData > cardList = [];
+		foreach (string filePath in DirAccess.GetFilesAt(CARD_DATA_PATH))
+		{
+			if (filePath.EndsWith(".tres"))
+			{
+				GD.Print(filePath);
+				var cardData = GD.Load<CardData>(CARD_DATA_PATH + filePath);
+				if (cardData != null)
+				{
+					cardList.Add(cardData);
+				}
+			}
+		}
+
+		return cardList;
 	}
 }
