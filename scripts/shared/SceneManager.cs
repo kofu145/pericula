8b473a3c836dcc6bd88e80e3d194bbfbd542e8cc
Bubseatbubs <bubseatbubs@gmail.com>
2025-08-28 12:15:12 -0700

    Feat: Implement diamond-style scene transition between scenes

--- scripts/shared/SceneManager.cs
+++ scripts/shared/SceneManager.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 public partial class SceneManager : Node
 {
+	[Export] ColorRect SceneTransitionAnimationRect;
 	public static bool ChangingScenes = false;
 	public static string CurrentScene;
 	static SceneManager Instance;
@@ -29,9 +30,36 @@ public partial class SceneManager : Node
 		}
 	}
 
-	private void ChangeSceneHelper(string target)
+	private async void ChangeSceneHelper(string target)
 	{
+		SceneTransitionAnimationRect.MouseFilter = Control.MouseFilterEnum.Stop;
+
+		var TweenFade = CreateTween();
+		TweenFade.SetParallel(true);
+		TweenFade.TweenProperty(
+			SceneTransitionAnimationRect.Material,
+			"shader_parameter/progress",
+			0.7,
+			0.5f
+		).From(0f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+
+		await ToSignal(TweenFade, "finished");
+
 		GetTree().ChangeSceneToFile(target);
 		CurrentScene = target;
+		
+        var TweenUnfade = CreateTween();
+        TweenUnfade.TweenProperty(
+            SceneTransitionAnimationRect.Material,
+            "shader_parameter/progress",
+            0,
+            0.75f
+        ).From(0.75f).SetTrans(Tween.TransitionType.Sine);
+
+        SceneTransitionAnimationRect.MouseFilter = Control.MouseFilterEnum.Ignore;
+        GetTree().ChangeSceneToFile(target);
+
+        await ToSignal(TweenUnfade, "finished");
+        ChangingScenes = false;
 	}
 }
