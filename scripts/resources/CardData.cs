d400af2b18da24192e2708fbb22c48035ba1ac23
Kofu <kofu145g@gmail.com>
2025-08-28 15:51:25 -0700

    fix(carddata):quickfix mismatch from merge

--- scripts/resources/CardData.cs
+++ scripts/resources/CardData.cs
@@ -12,7 +12,7 @@ public partial class CardData : Resource
 	[Export(PropertyHint.Enum, "Knight,Arcane,Citizen,Royalty,Beast,Mechanical,WildCard,Pawn")]
 	public string Trait;
 	[Export] public int HP;
-	public int MaxHP;
+	public int BaseHP;
 	public int BaseAttack;
 	[Export] public int Attack;
 	[Export] public int id;
@@ -21,10 +21,10 @@ public partial class CardData : Resource
 	[Export] public Array<EffectTemplate> Passives;
 
 	CardData()
-    {
-        BaseHP = HP;
-        BaseAttack = Attack;
-    }
+	{
+		BaseHP = HP;
+		BaseAttack = Attack;
+	}
 
 	public override string ToString()
 	{
