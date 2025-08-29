7273ec8602ecececef71751fd749d9c327108d9a
Bubseatbubs <bubseatbubs@gmail.com>
2025-08-29 00:37:18 -0700

    Feat: Add settings menu

---
+++ scripts/AudioBusSlider.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class AudioBusSlider : Node
+{
+    [Export] string BusName;
+    int BusIndex;
+
+    public override void _Ready()
+    {
+        BusIndex = AudioServer.GetBusIndex(BusName);
+        base._Ready();
+    }
+
+    public void OnValueChanged(float value)
+    {
+        AudioServer.SetBusVolumeDb(
+            BusIndex,
+            Mathf.LinearToDb(value)
+        );
+    }
+
+    public void SoundFinishedChanging(bool value_changed)
+    {
+        SoundManager.PlaySE("FX01");
+    }
+}
