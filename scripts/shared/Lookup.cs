6d081e34abe5b114b189f9698f8ce85f633ef6ed
Kofu <kofu145g@gmail.com>
2025-08-30 03:44:52 -0700

    feat(shop): shop correctly generates cards based on rarity weights

--- scripts/shared/Lookup.cs
+++ scripts/shared/Lookup.cs
@@ -38,6 +38,8 @@ public partial class Lookup : Node
 
     public static Godot.Collections.Array<CardData> GetCardsByRarity(RarityType rarity)
     {
+
+        GD.Print(rarity);
         Godot.Collections.Array<CardData> result = new();
         foreach (var card in lookupList)
         {
