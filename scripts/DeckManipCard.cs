e4c625f252e7a0d4c5374376c4b08f0b26560290
Bubson <bubseatbubs@gmail.com>
2025-08-29 22:07:01 -0700

    Fix: Update Too Expensive text

--- scripts/DeckManipCard.cs
+++ scripts/DeckManipCard.cs
@@ -52,12 +52,15 @@ public partial class DeckManipCard : Control
     {
         if (_disabled)
         {
-            PopupText.Instance.ShowText(GlobalPosition, "Too expensive!");
             return;
         }
         else if (ChipManager.Instance.Balance >= _deckManipID)
         {
             Buy();
+        }
+        else
+        {
+            PopupText.Instance.ShowText(GlobalPosition, "Too expensive!");
         }
     }
 
