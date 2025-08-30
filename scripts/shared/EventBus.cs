24a6e0a72b17a91b2598c60442994c0cd1ff242d
Kofu <kofu145g@gmail.com>
2025-08-30 11:37:52 -0700

    feat(eventbus): edit with multiple common combat events

--- scripts/shared/EventBus.cs
+++ scripts/shared/EventBus.cs
@@ -9,17 +9,24 @@ public partial class EventBus : Node
         Instance = this;
     }
 
+    [Signal]
+    public delegate void TriggeredEventHandler();
+
     //[Signal]
     public delegate void EventHandler();
 
     //public event EventHandler OnDamage;
 
     public event Action RefreshBattleLoop;
+    public event Action ShowdownEvent;
+    public event Action AdvantageEvent;
+    public event Action UnobscuredEvent;
+    public event Action FinalWager;
 
-    public void RefreshBattle()
-    {
-        RefreshBattleLoop?.Invoke();
-    }
+    public void RefreshBattle() => RefreshBattleLoop?.Invoke();
+    public void InvokeAdvantage() => AdvantageEvent?.Invoke();
+    public void InvokeObscured() => UnobscuredEvent?.Invoke();
+    public void InvokeShowdown() => ShowdownEvent?.Invoke();
 
     public void ClearEvents()
     {
