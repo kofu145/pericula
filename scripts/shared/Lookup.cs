ad3abcd3685ef25b91e09fd61df415a7f76809e4
Kofu <kofu145g@gmail.com>
2025-08-30 03:17:33 -0700

    feat(lookup): changed fetch instance data to happen on ready rather than on every method call for getcardbyid

--- scripts/shared/Lookup.cs
+++ scripts/shared/Lookup.cs
@@ -9,27 +9,45 @@ public partial class Lookup : Node
     const string CARD_DATA_PATH = "res://resources/cardData/";
     const string DECK_MANIP_PATH = "res://resources/deckManipData/";
 
+    private static Godot.Collections.Array<CardData> lookupList = new();
+
     public override void _Ready()
     {
         Instance = this;
+        foreach (string filepath in DirAccess.GetFilesAt(CARD_DATA_PATH))
+        {
+            if (filepath.EndsWith(".tres"))
+            {
+                var cardData = GD.Load<CardData>(CARD_DATA_PATH + filepath);
+                lookupList.Add(cardData);
+            }
+        }
     }
 
     public static CardData GetCardByID(int id)
     {
-        foreach (string filePath in DirAccess.GetFilesAt(CARD_DATA_PATH))
+        foreach (var card in lookupList)
         {
-            if (filePath.EndsWith(".tres"))
-            {
-                var cardData = GD.Load<CardData>(CARD_DATA_PATH + filePath);
-                if (cardData != null && cardData.id == id)
-                {
-                    return (CardData)cardData.Duplicate(true);
-                }
-            }
+            if (card.id == id)
+                return card;
         }
 
         GD.PrintErr("Couldn't find card with ID: " + id);
         return null;
+    }
+
+    public static Godot.Collections.Array<CardData> GetCardsByRarity(RarityType rarity)
+    {
+        Godot.Collections.Array<CardData> result = new();
+        foreach (var card in lookupList)
+        {
+            if (card.Rarity.RarityType == rarity)
+            {
+                result.Add(card);
+            }
+        }
+
+        return result;
     }
 
     public static DeckManipData GetDeckManipByID(int id)
