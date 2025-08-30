47343f387c99efd6566f459ff1b5fad3b7a87d29
Kofu <kofu145g@gmail.com>
2025-08-30 12:54:18 -0700

    chore: rename pawnadv buff eff to showdown eff, because spade pawn is not on advantage

--- scripts/resources/effect/PawnShowdownBuffEffect.cs
+++ scripts/resources/effect/PawnShowdownBuffEffect.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 
 [GlobalClass]
-public partial class PawnShowdownBuffEffect : EffectTemplate
+public partial class PawnAdvantageBuffEffect : EffectTemplate
 {
     [Export] public int HPBuff = 2;
     [Export] public int DamageBuff = 2;
