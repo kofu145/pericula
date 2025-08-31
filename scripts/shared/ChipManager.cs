4abbc099cf58ec8961b363f779b3b8126ca04b33
Bubson <bubseatbubs@gmail.com>
2025-08-30 20:42:44 -0700

    Feat: Implement chips drop sound

--- scripts/shared/ChipManager.cs
+++ scripts/shared/ChipManager.cs
@@ -42,6 +42,8 @@ public partial class ChipManager : Node
             OnChipsChanged?.Invoke(balance);
             ChipsUsed += amount;
         }
+
+        SoundManager.PlaySE("chipsdrop");
         return valid;
     }
 
