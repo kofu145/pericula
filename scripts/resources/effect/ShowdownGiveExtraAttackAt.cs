569a0e84cfd7a77ae53e219d60a021c9f7e213e6
Ralph Alexander <ralph.halim103@gmail.com>
2025-08-31 01:44:04 -0700

    feat: implemented delay between betting turns

--- scripts/resources/effect/ShowdownGiveExtraAttackAt.cs
+++ scripts/resources/effect/ShowdownGiveExtraAttackAt.cs
@@ -3,4 +3,26 @@ using System;
 
 public partial class ShowdownGiveExtraAttackAt : EffectTemplate
 {
+    [Export] public int targetIndex = 0;
+
+    public override void Initialize(EffectParam param)
+    {
+        Action handler = null;
+        handler = () =>
+        {
+            param.State.QueueTrigger(
+             async () =>
+            {
+                var lane = param.State.GetSide(param.Self);
+                var card = lane.GetCardAtIndex(targetIndex);
+                // card.
+                // give attack 
+
+                await DoTriggerAnimation(param);
+            });
+            EventBus.Instance.ShowdownEvent -= handler;
+        };
+
+        EventBus.Instance.ShowdownEvent += handler;
+    }
 }
