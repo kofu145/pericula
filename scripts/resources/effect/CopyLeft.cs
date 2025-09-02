16c88093f3db17eb110355ab58573ef1c6d0a2af
Kofu <kofu145g@gmail.com>
2025-09-01 17:53:05 -0700

    fix: joker bug with not being updated with shieldbearer id

--- scripts/resources/effect/CopyLeft.cs
+++ scripts/resources/effect/CopyLeft.cs
@@ -12,17 +12,27 @@ public partial class CopyLeft : EffectTemplate
         this.param = param;
 
         Reset();
+        GD.Print("start of pre init");
         var side = param.State.GetSide(param.Self);
         var idx = side.IndexOf(side.GetCardBaseByData(param.Self));
-        if (idx - 1 >= 0 && side.GetCardAtIndex(idx - 1).id != param.Self.id)
+        try
         {
-            int copyId = side.GetCardAtIndex(idx - 1).id;
-            var copyBase = (CardData)Lookup.GetCardByID(copyId).Duplicate(true);
-            param.Self.OnUse.AddRange(copyBase.OnUse.Duplicate(true));
-            param.Self.Passives.AddRange(copyBase.Passives.Duplicate(true));
-            param.Self.Attack = copyBase.BaseAttack;
-            param.Self.HP = copyBase.BaseHP;
+            if (idx - 1 >= 0 && side.GetCardAtIndex(idx - 1).id != param.Self.id)
+            {
+                int copyId = side.GetCardAtIndex(idx - 1).id;
+                var copyBase = (CardData)Lookup.GetCardByID(copyId).Duplicate(true);
+                param.Self.OnUse.AddRange(copyBase.OnUse.Duplicate(true));
+                param.Self.Passives.AddRange(copyBase.Passives.Duplicate(true));
+                param.Self.Attack = copyBase.BaseAttack;
+                param.Self.HP = copyBase.BaseHP;
+            }
         }
+        catch (System.Exception ex)
+        {
+            GD.PrintErr($"err in preinit {ex.Message}");
+            GD.PushError($"Full error details: {ex.ToString()}");
+        }
+        GD.Print("end of pre in it");
     }
 
     public override void Initialize(EffectParam param)
