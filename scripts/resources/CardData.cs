af05be378593af4e0cb550ba83f3674a905643ff
Ralph Alexander <ralph.halim103@gmail.com>
2025-09-03 01:34:31 -0700

    fix: fixed a bug where joker cannot buff itself when copying king of diamond's effect

--- scripts/resources/CardData.cs
+++ scripts/resources/CardData.cs
@@ -96,6 +96,6 @@ public partial class CardData : CodexItemData
     /// <returns></returns>
     public bool IsTargetTrait(Trait targetTrait)
     {
-        return (Trait == targetTrait || Trait == Trait.WildCard);
+        return Trait == targetTrait || Trait == Trait.WildCard;
     }
 }
--- scripts/resources/effect/ShowdownBuffClass.cs
+++ scripts/resources/effect/ShowdownBuffClass.cs
@@ -8,6 +8,7 @@ public partial class ShowdownBuffClass : EffectTemplate
     [Export] public int HPBuff = 1;
     [Export] public int DamageBuff = 1;
     [Export] public Trait TraitToBuff = Trait.Arcane;
+    [Export] public bool CanTargetSelf = true;
 
     public override void Initialize(EffectParam param)
     {
@@ -20,7 +21,7 @@ public partial class ShowdownBuffClass : EffectTemplate
                 for (int i = 0; i < lane.CardCount; i++)
                 {
                     var card = lane.GetCardAtIndex(i);
-                    if (card.IsTargetTrait(TraitToBuff) && card != param.Self)
+                    if (card.IsTargetTrait(TraitToBuff) && (CanTargetSelf || card != param.Self))
                     {
                         card.HP += HPBuff;
                         card.Attack += DamageBuff;
